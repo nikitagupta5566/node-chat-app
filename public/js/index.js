7e423d42165c86bcbcefc94e307d13dd52700620
Nikita <nikitagupta5566@gmail.com>
2019-01-22 21:48:32 +0530

    Add newMeassage and createMessage events

---
+++ public/js/index.js
@@ -0,0 +1,32 @@
+var socket = io(); 
+// it is a method available to us because we loaded this library. It's not native to the browser. 
+//And when we call it we're actually initiating the request from the client to the server to open up a
+// web socket and keep that connection alive.
+
+socket.on('connect' ,function () {
+	console.log("Connected to the server");
+
+	socket.emit('createEmail', {
+		to: "mike@ngkfng.com",
+		text: "hi i am fine",
+	})
+
+	socket.emit('createMessage', {
+		to: 'nikita',
+		text: 'Can i call u up',
+	})
+
+})
+
+socket.on('disconnect', function () {
+	console.log("Disconnected from server");
+})
+
+socket.on('newEmail', function(email) {             // the data that is emmitted with your event is passed as the first argument to your function
+	console.log('New Email',email);
+})
+
+
+socket.on('newMessage', function(message) {
+	console.log('Hey! you have a new message',message);
+})
--- server/server.js
+++ server/server.js
@@ -1,5 +1,7 @@
 const path = require('path');
 const express = require('express');
+const socketIO = require('socket.io');
+const http = require('http');
 
 var publicPath = path.join(__dirname, '../public');
 console.log(publicPath);   // returns the final path
@@ -7,9 +9,44 @@ console.log(publicPath);   // returns the final path
 // console.log(__dirname + '../public');    // F:\NodeProjects\node-chat-app\server../public (returns path like this)
 
 var app = express();
+var server = http.createServer(app);
+var io = socketIO(server);		// Pass the server that we want to use with socketio
 
+// io is a web socket server.
 app.use(express.static(publicPath));
 
-app.listen(3000, () => {
+
+// io.on lets u register event listener. We can listen for a specific event and do sthg when that event happens.
+// connection is an event that lets u listen for a new connection an let u do sthg when that connection is established.
+
+io.on('connection', (socket) => {
+	console.log("New user connected");
+
+	socket.on('disconnect', () => {
+		console.log("Disconnected from the client");
+	})
+
+	socket.on('createEmail', function(email) {
+		console.log(email);
+	})
+
+	socket.on('createMessage', function(message) {
+		console.log(message);
+	})
+
+	socket.emit('newEmail', {
+		from: "nikitagupta",
+		text: "Hi how r u",
+		createAt: Date()
+	}); // second argument is the data that we want to pass....since we want to send multiple data we are going to pass an object
+
+	socket.emit('newMessage', {
+		from: 'Nikita',
+		text: 'Yeah, Sure',
+	})
+})
+
+
+server.listen(3000, () => {
 	console.log("Server is up on port 3000");
 })
